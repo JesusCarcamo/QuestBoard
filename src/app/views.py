62363bc0ae5a0d3cde9aafda90c05c43176e72d2
Berserker1523 <berserker1523@hotmail.com>
2019-04-03 17:34:30 -0500

    Lo que fue, fue

--- src/app/views.py
+++ src/app/views.py
@@ -4,12 +4,17 @@ from django.conf import settings
 # Create your views here.
 def inicio(request):
     context = {
-
     }
     return render(request, "inicio.html",context)
 
 def ingresar(request):
     context = {
-
     }
     return render(request, "ingresar.html",context)
+
+def perfil(request):
+    context = {
+    }
+    return render(request, "perfil.html",context)
+
+
