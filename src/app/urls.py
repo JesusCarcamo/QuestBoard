e3fbe4ef2ff636e040de2ec500091f732c657178
unknown <l.mejia@uniandes.edu.co>
2019-04-22 14:44:07 -0500

    progress en los servicios

--- src/app/urls.py
+++ src/app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicio, name='inicio')
+    path('', views.inicio, name='inicio'),
+    path('jugadores', views.jugadores_list),
+    path('misiones', views.misiones_list),
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -1,8 +1,67 @@
 from django.shortcuts import render
 from django.conf import settings
+from .serializers import JugadorSerializer, MisionSerializer
+from rest_framework.parsers import JSONParser
+from .models import Jugador, Mision, Juego, Dia
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 def inicio(request):
     return render(request, "base.html")
+
+
+@csrf_exempt
+def jugadores_list(request):
+    # Metodo que responde y envia peticiones en forma de lista de Jsons correspondientes con el modelo del producto
+    if request.method == 'GET':
+        # En caso de recibir una peticion de tipo GET
+        # Solicita todos los productos a la base de datos
+        jugadores = Jugador.objects.all()
+        # Convierte el listado de modelos de productos a un listado de productos JSON
+        serializer = JugadorSerializer(jugadores, many=True)
+        # Retorna el listado de productos JSON en forma de respuesta HTTP
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        # En caso de recibir una peticion POST
+        # Convierte el JSon en un objeto de Python
+        data = JSONParser().parse(request)
+        # Convierte el objeto en un modelo de Producto
+        serializer = JugadorSerializer(data=data)
+        # Comprueba la validez del modelo
+        if serializer.is_valid():
+            # Guarda el objeto en la base de datos
+            serializer.save()
+            # Retorna el nuevo Objeto almacendao en la base de datos
+            return JsonResponse(serializer.data, status=201)
+        # Retorna una respuest de fallo
+        return JsonResponse(serializer.errors, status=404)
+
+@csrf_exempt
+def misiones_list(request):
+    # Metodo que responde y envia peticiones en forma de lista de Jsons correspondientes con el modelo del producto
+    if request.method == 'GET':
+        # En caso de recibir una peticion de tipo GET
+        # Solicita todos los productos a la base de datos
+        misiones = Jugador.objects.all()
+        # Convierte el listado de modelos de productos a un listado de productos JSON
+        serializer = MisionSerializer(misiones, many=True)
+        # Retorna el listado de productos JSON en forma de respuesta HTTP
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        # En caso de recibir una peticion POST
+        # Convierte el JSon en un objeto de Python
+        data = JSONParser().parse(request)
+        # Convierte el objeto en un modelo de Producto
+        serializer = MisionSerializer(data=data)
+        # Comprueba la validez del modelo
+        if serializer.is_valid():
+            # Guarda el objeto en la base de datos
+            serializer.save()
+            # Retorna el nuevo Objeto almacendao en la base de datos
+            return JsonResponse(serializer.data, status=201)
+        # Retorna una respuest de fallo
+        return JsonResponse(serializer.errors, status=404)
+
 
 
